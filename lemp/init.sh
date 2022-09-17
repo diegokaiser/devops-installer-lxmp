4c769464f519ac51157db70f8e1190c151d5cbbc
Diego Cáceres Cardoza <diegokaiser@gmail.com>
2022-09-17 19:05:28 +0200

    version 1.1.2, referencia ruta home, sin usuario

--- lemp/init.sh
+++ lemp/init.sh
@@ -4,30 +4,30 @@
 echo "Heil, mein Kaiser!"
 
 # updating and upgrading
-~/$USER/devops-installer-lxmp/tools/updateNupgrade.sh
+~/devops-installer-lxmp/tools/updateNupgrade.sh
 
 # swaping
-~/$USER/devops-installer-lxmp/tools/swap.sh
+~/devops-installer-lxmp/tools/swap.sh
 
 echo "lemping..."
 # installing nginx
-~/$USER/devops-installer-lxmp/tools/nginx.sh
+~/devops-installer-lxmp/tools/nginx.sh
 
 # installing mysql
-~/$USER/devops-installer-lxmp/tools/mysql.sh
+~/devops-installer-lxmp/tools/mysql.sh
 
 # installing php
-~/$USER/devops-installer-lxmp/tools/php.sh
+~/devops-installer-lxmp/tools/php.sh
 echo "lemping done!"
 
 # owning
-~/$USER/devops-installer-lxmp/tools/fixes/lxmp_owning.sh
+~/devops-installer-lxmp/tools/fixes/lxmp_owning.sh
 
 # users and permissions
-~/$USER/devops-installer-lxmp/tools/fixes/lxmp_permissions.sh
+~/devops-installer-lxmp/tools/fixes/lxmp_permissions.sh
 
 # UFW
-~/$USER/devops-installer-lxmp/tools/ufw.sh
+~/devops-installer-lxmp/tools/ufw.sh
 
 # finish...
-~/$USER/devops-installer-lxmp/tools/reboot.sh
+~/devops-installer-lxmp/tools/reboot.sh
