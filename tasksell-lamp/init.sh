d6698bb07be3f82a083f605525e9b86571def02e
Diego Cáceres Cardoza <diegokaiser@gmail.com>
2022-09-17 19:12:03 +0200

    actualizando script para tasksellamp

--- tasksell-lamp/init.sh
+++ tasksell-lamp/init.sh
@@ -4,30 +4,30 @@
 echo "Heil, mein Kaiser!"
 
 # updating and upgrading
-/$USER/devops-installer-lxmp/tools/updateNupgrade.sh
+~/devops-installer-lxmp/tools/updateNupgrade.sh
 
 # swaping
-/$USER/devops-installer-lxmp/tools/swap.sh
+~/devops-installer-lxmp/tools/swap.sh
 
 echo "lamping..."
-/$USER/devops-installer-lxmp/tools/tasksel_lamp.sh
+~/devops-installer-lxmp/tools/tasksel_lamp.sh
 
 echo "installing libraries..."
 # installing libraries
-/$USER/devops-installer-lxmp/tools/mods/php_libraries.sh
+~/devops-installer-lxmp/tools/mods/php_libraries.sh
 echo "lamping done!"
 
 # enabling mod rewrite
-/$USER/devops-installer-lxmp/tools/mods/mod_rewrite.sh
+~/devops-installer-lxmp/tools/mods/mod_rewrite.sh
 
 # owning
-/$USER/devops-installer-lxmp/tools/fixes/lxmp_owning.sh
+~/devops-installer-lxmp/tools/fixes/lxmp_owning.sh
 
 # users and permissions
-/$USER/devops-installer-lxmp/tools/fixes/lxmp_permissions.sh
+~/devops-installer-lxmp/tools/fixes/lxmp_permissions.sh
 
 # UFW
-/$USER/devops-installer-lxmp/tools/ufw.sh
+~/devops-installer-lxmp/tools/ufw.sh
 
 # finish...
-/$USER/devops-installer-lxmp/tools/reboot.sh
+~/devops-installer-lxmp/tools/reboot.sh
